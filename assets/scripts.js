97a7f3b6822249fc3ef8b1227ed56665752359b8
Kenneth Gitere <gitere81@gmail.com>
2017-02-25 08:08:38 +0300

    added skip functionality

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,14 +14,26 @@ $(document).ready(function () {
     $("p#intro-cont").delay(21000).fadeIn(2000);
     $("div#intro").delay(4000).fadeOut(2000);
     $("div#intro-story").delay(29000).fadeIn(2000);
-    $("div#intro-story").fadeOut(2000);
-    $("div#intro-story1").delay(35000).fadeIn(2000);
+    $("div#intro-story").delay(2000).fadeOut(2000);
+    $("div#intro-story1").delay(36000).fadeIn(2000);
     $("div#intro-story1").fadeOut(2000);
-    $("div#intro-story2").delay(40000).fadeIn(2000);
+    $("div#intro-story2").delay(41000).fadeIn(2000);
     $("div#intro-story2").delay(2000).fadeOut(2000);
-    $("div#intro-story3").delay(47000).fadeIn(2000);
+    $("div#intro-story3").delay(48000).fadeIn(2000);
     $("div#intro-story3").fadeOut(2000);
-    $("div#welcome").delay(52000).fadeIn(2000);
-    // $("div#welcome").fadeOut(2000);
+    $("div#welcome").delay(53000).fadeIn(2000);
+    $("button#enter").delay(56000).fadeIn(2000);
+  });
+  $("button#enter").click(function () {
+    $("div#welcome").delay(3000).fadeOut(2000);
+    $("body").removeClass();
+    $("body").addClass("homepage");
+    $("div#homepage").delay(5000).fadeIn(2000);
+  });
+  $("button#skip").click(function () {
+    $("div#landing").delay(3000).fadeOut(2000);
+    $("body").removeClass();
+    $("body").addClass("homepage-skip");
+    $("div#homepage").delay(5000).fadeIn(2000);
   });
 });
