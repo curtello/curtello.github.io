566b8abd722f2bbfe4b3d1dbc4cba921d5bb5155
Kenneth Gitere <gitere81@gmail.com>
2017-02-25 21:54:32 +0300

    finished About Me

--- assets/scripts.js
+++ assets/scripts.js
@@ -48,8 +48,41 @@ $(document).ready(function () {
     $("div#Contacts").delay(13000).fadeIn(1000);
   });
   $("div#About").click(function () {
-    $("div#About").fadeOut(2000);
-    $("div#Projects").fadeOut(2000);
-    $("div#Contacts").fadeOut(2000);
+    $("div#About").slideUp(2000);
+    $("div#Projects").slideUp(2000);
+    $("div#Contacts").slideUp(2000);
+    $("div#About-desc").slideDown(2000);
+    $("button#about-return").slideDown(2000);
+  });
+  $("div#me").click(function () {
+    $("div#me").slideUp(2000);
+    $("div#more-on-me").slideDown(2000);
+  });
+  $("div#hobbies").click(function () {
+    $("div#hobbies").slideUp(2000);
+    $("div#more-on-hobbies").slideDown(2000);
+  });
+  $("div#skill-sets").click(function () {
+    $("div#skill-sets").slideUp(2000);
+    $("div#more-on-skill-sets").slideDown(2000);
+  });
+  $("button#me-shut").click(function () {
+    $("div#more-on-me").slideUp(2000);
+    $("div#me").slideDown(2000);
+  });
+  $("button#hobbies-shut").click(function () {
+    $("div#more-on-hobbies").slideUp(2000);
+    $("div#hobbies").slideDown(2000);
+  });
+  $("button#skill-sets-shut").click(function () {
+    $("div#more-on-skill-sets").slideUp(2000);
+    $("div#skill-sets").slideDown(2000);
+  });
+  $("button#about-return").click(function () {
+    $("button#about-return").slideUp(2000);
+    $("div#About-desc").slideUp(2000);
+    $("div#About").slideDown(2000);
+    $("div#Projects").slideDown(2000);
+    $("div#Contacts").slideDown(2000);
   });
 });
